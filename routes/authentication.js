e1255fcb028f1d7bcbe24292c506f50475416c82
beneditaalmeida <benedita@MacBook-Pro-de-Benedita.local>
2019-09-10 14:21:17 +0100

    work in progress

--- routes/authentication.js
+++ routes/authentication.js
@@ -5,6 +5,14 @@ const router = Router();
 const User = require('./../models/user');
 const bcrypt = require('bcrypt');
 
+const routeGuardMiddleware = (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect('/authentication/sign-in');
+  } else {
+    next();
+  }
+};
+
   
 router.get('/sign-up', (req, res, next) => {
     res.render('sign-up');
@@ -33,18 +41,54 @@ router.get('/sign-up', (req, res, next) => {
       });
   });
 
+  router.get('/login', (req, res, next) => {
+    res.render('login');
+  });
 
-router.get('/login', (req, res, next) => {
-  res.render('login');
+
+  router.post('/login', (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    let auxiliaryUser;
+  
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          throw new Error('USER_NOT_FOUND');
+        } else {
+          auxiliaryUser = user;
+          return bcrypt.compare(password, user.passwordHash);
+        }
+      })
+      .then(matches => {
+        if (!matches) {
+          throw new Error('PASSWORD_DOESNT_MATCH');
+        } else {
+          req.session.user = {
+            _id: auxiliaryUser._id
+          };
+          res.redirect('private');
+        }
+      })
+      .catch(error => {
+        console.log('There was an error signing up the user', error);
+        next(error);
+      });
+  });
+
+router.get('/private', routeGuardMiddleware, (req, res, next) => {
+  res.render('private');
 });
 
-router.post('/login', (req, res, next) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  console.log(email, password);
-  res.redirect('/authentication/private');
-});
 
+router.get('/private', (req, res, next) => {
+    res.render('private');
+  });
+
+
+
+module.exports = router;
 
   // const bcryptSalt = 10;
 
@@ -78,12 +122,3 @@ router.post('/login', (req, res, next) => {
 //         next(error);
 //     });
 // });
-
-
-router.get('/private', (req, res, next) => {
-    res.render('private');
-  });
-
-
-
-module.exports = router;
